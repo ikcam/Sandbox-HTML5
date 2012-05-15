f5fcfe2ba3eb2ce43073a00bd08b15962eea5e80
Irving Kcam <xploit29@gmail.com>
2012-05-14 22:19:14 -0500

    Sidebars file

---
+++ inc/sidebars.php
@@ -0,0 +1,19 @@
+<?php
+	$sidebar = array(
+		'name'           =>   __( 'Primary Sidebar', 'Sandbox' ),
+		'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
+		'after_widget'   =>   "\n\t\t\t</li>\n",
+		'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
+		'after_title'    =>   "</h3>\n"
+	);
+	register_sidebar( $sidebar );
+
+	$sidebar = array(
+		'name'           =>   __( 'Secondary Sidebar', 'Sandbox' ),
+		'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
+		'after_widget'   =>   "\n\t\t\t</li>\n",
+		'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
+		'after_title'    =>   "</h3>\n"
+	);
+	register_sidebar( $sidebar );
+?>

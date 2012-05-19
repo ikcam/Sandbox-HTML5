df380fa941b7c0d05e18b92bf02f8f51e4ae3702
Irving Kcam <xploit29@gmail.com>
2012-05-18 23:00:56 -0500

    No sidebar page

---
+++ page-full.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: No Sidebar Page
+*/
+?>
+<?php get_header() ?>
+
+	<section id="container-full">
+		<section id="content">
+
+<?php the_post() ?>
+
+			<article id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+				<h2 class="entry-title"><?php the_title() ?></h2>
+				<div class="entry-content">
+<?php the_content() ?>
+
+<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
+
+<?php edit_post_link( __( 'Edit', 'sandbox' ), '<span class="edit-link">', '</span>' ) ?>
+
+				</div>
+			</article><!-- .post -->
+
+<?php if ( get_post_custom_values('comments') ) comments_template() // Add a key+value of "comments" to enable comments on this page ?>
+
+		</section><!-- #content -->
+	</section><!-- #container -->
+
+<?php get_footer() ?>

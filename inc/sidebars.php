d885687b8f2ecea81191ae2162aa9a2e1add0f58
Irving Kcam <xploit29@gmail.com>
2012-08-24 11:21:28 -0500

    Layouts sidebars

--- inc/sidebars.php
+++ inc/sidebars.php
@@ -1,86 +1,110 @@
 <?php
-$sidebar = array(
-	'name'           =>   __( 'Primary Sidebar', 'Sandbox' ),
-	'id'             =>   "primary",
-	'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
-	'after_widget'   =>   "\n\t\t\t</li>\n",
-	'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
-	'after_title'    =>   "</h3>\n"
-);
-register_sidebar( $sidebar );
+$settings = get_option('sb_settings');
 
-$sidebar = array(
-	'name'           =>   __( 'Secondary Sidebar', 'Sandbox' ),
-	'id'             =>   "secondary",
-	'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
-	'after_widget'   =>   "\n\t\t\t</li>\n",
-	'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
-	'after_title'    =>   "</h3>\n"
-);
-register_sidebar( $sidebar );
+if( $settings['site_layout'] != 6 ) :
+	$sidebar = array(
+			'name'           =>   __( 'Primary Sidebar', 'Sandbox' ),
+			'id'             =>   "primary",
+			'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
+			'after_widget'   =>   "\n\t\t\t</li>\n",
+			'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
+			'after_title'    =>   "</h3>\n"
+		);
+	register_sidebar( $sidebar );
 
-$footer_sidebars = get_option('sb_footer_sidebars');
+	if( $settings['site_layout'] == 3 || $settings['site_layout'] == 4 || $settings['site_layout'] == 5 ) :
+		$sidebar = array(
+				'name'           =>   __( 'Secondary Sidebar', 'Sandbox' ),
+				'id'             =>   "secondary",
+				'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
+				'after_widget'   =>   "\n\t\t\t</li>\n",
+				'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
+				'after_title'    =>   "</h3>\n"
+			);
+		register_sidebar( $sidebar );
+	endif; // Site layout: 3, 4, 5
+endif; // Site layout: 6
+
+$footer_sidebars = $settings['footer_layout'];
+if ( $footer_sidebars == 5 || $footer_sidebars == 6 )
+	$footer_sidebars = 2;
 
 for($i=1;$i<=$footer_sidebars;$i++){
 	$sidebar = array(
-		'name'           =>   sprintf( __( 'Footer Sidebar %d', 'Sandbox' ), $i ),
-		'id'             =>   "footer-$i",
-		'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
-		'after_widget'   =>   "\n\t\t\t</li>\n",
-		'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
-		'after_title'    =>   "</h3>\n"    
-	);
+			'name'           =>   sprintf( __( 'Footer Sidebar %d', 'Sandbox' ), $i ),
+			'id'             =>   "footer-$i",
+			'before_widget'  =>   "\n\t\t\t" . '<li id="%1$s" class="widget %2$s">',
+			'after_widget'   =>   "\n\t\t\t</li>\n",
+			'before_title'   =>   "\n\t\t\t\t". '<h3 class="widgettitle">',
+			'after_title'    =>   "</h3>\n"    
+		);
 	register_sidebar( $sidebar );
 }
 
 function sandbox_footer_sidebars(){
-	$count = get_option('sb_footer_sidebars');
+	$settings = get_option('sb_settings');
+	$layout = $settings['footer_layout'];
 	$class = 'footer-sidebar ';
 
-	switch ( $count ){
+	switch ( $layout ){
 		case 1:
+			$count = 1;
 			$class .= 'one_column';
 			break;
 		case 2:
+			$count = 2;
 			$class .= 'one_half';
 			break;
 		case 3:
+			$count = 3;
 			$class .= 'one_third';
 			break;
 		case 4:
+			$count = 4;
 			$class .= 'one_fourth';
+			break;
+		case 5:
+			$count = 2;
+			$class .= 'one_third';
+			break;
+		case 6:
+			$count = 2;
+			$class .= 'two_third';
 			break;
 		default:
 			return;
 	}
 
-	for($i=1;$i<=$count;$i++){
-		if($i!=$count){
+	for( $i=1;$i<=$count;$i++ ) {
+		$id = 'footer-'.$i;
+		if( $i != $count ) :
 ?>
-	<aside class="<?php echo $class ?>">
-		<ul>
-		<?php 
-			$id = 'footer-'.$i;
-			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( $id ) ) :
-			endif;
-		?>
-		</ul>
-	</aside>
-<?php
-		} else {
+			<aside class="<?php echo $class ?>">
+				<ul>
+				<?php 
+					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( $id ) ) :
+					endif;
+				?>
+				</ul>
+			</aside>
+<?php 
+		else : 
+			if( $layout == 5 )
+				$class = "footer-sidebar two_third";
+			if( $layout == 6 )
+				$class = "footer-sidebar one_third";
 ?>
-	<aside class="<?php echo $class ?> last">
-		<ul>
-		<?php 
-			$id = 'footer-'.$i;
-			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( $id ) ) :
-			endif;
-		?>
-		</ul>
-	</aside>
-	<div class="clearfix"></div>
-<?php
-		}
+			<aside class="<?php echo $class ?> last">
+				<ul>
+				<?php 
+					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( $id ) ) :
+					endif;
+				?>
+				</ul>
+			</aside>
+			<div class="clearfix"></div>
+<?php 
+		endif;
 	}
 }
 ?>

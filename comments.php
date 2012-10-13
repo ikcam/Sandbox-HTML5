e884883760b42d5d171847e1979de587f3438fb4
Irving Kcam <xploit29@gmail.com>
2012-10-13 11:03:22 -0500

    Remove one function

--- comments.php
+++ comments.php
@@ -10,7 +10,6 @@
 	endif;
 ?>
 <?php if ( have_comments() ) : ?>
-
 				<h3><?php comments_number('No Responses', 'One Respond', '% Responses' );?></h3>
 				<section id="comments-list" class="comments">
 					<?php wp_list_comments( 'type=comment&style=div' ); ?>
@@ -19,17 +18,11 @@
 						<div class="nav-next"><?php next_comments_link() ?></div>
 					</nav>
 				</section><!-- #comments-list .comments -->
-
 <?php endif // REFERENCE: if ( have_comments() ) ?>
-
-<?php if( sandbox_comments() ) : ?>
 <?php if ( comments_open() ) : ?>
-
 				<section id="respond">
 					<?php comment_form(); ?>
 				</section><!-- #respond -->
-				
 <?php endif // REFERENCE: if ( comments_open() ) ?>
-<?php endif // REFERENCE: if ( sandbox_comments() ) ?>
 			</section><!-- #comments -->
 <?php endif // REFERENCE: if ( sandbox_comments() ) ?>

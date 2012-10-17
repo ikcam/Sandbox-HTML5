306e75939af482cf7b4f2d97e1f300e531f75c8f
Irving Kcam <xploit29@gmail.com>
2012-10-17 16:52:06 -0500

    Footer info

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 
 	<footer id="footer">
 		<?php sandbox_footer_sidebars() ?>
-		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
+
+		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator">WordPress</a></span>
 		<span class="meta-sep">|</span>
-		<span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
+		<span id="theme-link"><a href="https://github.com/ikcam/Sandbox-HTML5" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer">Sandbox</a></span>
 	</footer><!-- #footer -->
 
 </section><!-- #wrapper .hfeed -->

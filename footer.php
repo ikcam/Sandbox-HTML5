7040387fad31f84b7fe2c56a0b3e05d1ec203183
Irving Kcam <xploit29@gmail.com>
2012-10-21 10:36:29 -0500

    Relocate Google Analitycs

--- footer.php
+++ footer.php
@@ -5,11 +5,23 @@
 		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator">WordPress</a></span>
 		<span class="meta-sep">|</span>
 		<span id="theme-link"><a href="https://github.com/ikcam/Sandbox-HTML5" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer">Sandbox</a></span>
+		<?php wp_footer() ?>
 	</footer><!-- #footer -->
 
 </section><!-- #wrapper .hfeed -->
 
-<?php wp_footer() ?>
+<?php if( $settings['site_ga'] != '' ) : ?>
+<script>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo $settings["site_ga"] ?>']);
+  _gaq.push(['_trackPageview']);
 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<?php endif; ?>
 </body>
 </html>
